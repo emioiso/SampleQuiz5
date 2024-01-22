e4730fb514939faf77087cb02f9240f9ba74b734
emioiso <jijjjiijiijijiji@gmail.com>
2024-01-22 20:29:01 +0900

    Initial Commit

---
+++ SampleQuiz5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleQuiz5
+//
+//  Created by emi oiso on 2024/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
